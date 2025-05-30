3c1db64fe5b8f19779365878133455a1f9d79cc5
4C01 t <Catcat-jia@foxmail.com>
2025-05-30 20:12:03 +0800

    fix: forgot main class

---
+++ src/main/java/win/trystage/autofly/AutoFly.java
@@ -0,0 +1,20 @@
+package win.trystage.autofly;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.List;
+
+public final class AutoFly extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+        new File(this.getDataFolder(), "config.yml");
+        String permission = this.getConfig().getString("permission");
+        String message = this.getConfig().getString("message");
+        boolean byDefault = this.getConfig().getBoolean("byDefault");
+        List<String> worlds = this.getConfig().getStringList("worlds");
+        this.getServer().getPluginManager().registerEvents(new Listeners(permission, message, byDefault, worlds), this);
+    }
+}
